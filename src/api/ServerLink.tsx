1781c6f58c17e7d362ca81531961f325bf0a7ea7
リカルド <ric202cjc@gmail.com>
2025-02-23 13:48:47 +0900

    added customermaster

--- src/api/ServerLink.tsx
+++ src/api/ServerLink.tsx
@@ -1,2 +1,2 @@
 export const apihost = "http://localhost:3000/";
-export const apigeocodingapikey = "Au4NUBWJ_WQn2L6HUiPcdlZ31AlcABRjboVbqY5S80-R1B_nZUJX6dO9OTCAJ6AC";
+export const apigeocodingapikey = "Au4NUBWJ";
